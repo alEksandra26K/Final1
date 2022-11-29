74a0f5789b1e5ad825ec3c73be21c827cd3d1d77
Александра <belka_strelka_96@mail.ru>
2022-11-29 23:48:28 +0300

    first commit1

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите элементы массива через пробел");
+string[] array = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] array2 = NewArray(array);
+Console.WriteLine("Ваш новый массив");
+PrintArray(array2);
+
+void PrintArray(string[] array1)
+{
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+}
+
+
+
+string[] NewArray(string[] array1)
+{
+    int j = 0;
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            j = j + 1;
+        }
+    }
+    string[] arraynew = new string[j + 1];
+    j = 0;   
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            arraynew[j] = array1[i];
+            j = j + 1;
+        }
+    }
+    return arraynew;
+}
